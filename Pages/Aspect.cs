c7cfbd1536aee2cdc000de6f479446c93c921683
reesh <reesh@zerprize.com>
2023-02-17 14:08:30 +1300

    Add project files.

---
+++ Pages/Aspect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    public class Aspect
+    {
+        private string type;
+        private string index;
+        private string orphan;
+        private string name;
+        private string fnIDNr;
+
+        public Aspect(string type, string index, string orphan, string name, string fnIDNr)
+        {
+            this.type = type;
+            this.index = index;
+            this.orphan = orphan;
+            this.name = name;
+            this.fnIDNr = fnIDNr;
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public string IDNr
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        public string Orphan
+        {
+            get { return orphan; }
+            set { orphan = value; }
+        }
+        public string IDName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string FunctionIDNr
+        {
+            get { return fnIDNr; }
+            set { fnIDNr = value; }
+        }
+    }
